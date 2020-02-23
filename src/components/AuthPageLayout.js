f8df305f2396ef9292a8ba4daf7e85da8c46edd4
mahmoudjbour <mjbour@kiitos-tech.com>
2020-02-23 20:21:55 +0200

    filters

--- src/components/AuthPageLayout.js
+++ src/components/AuthPageLayout.js
@@ -1,16 +1,20 @@
-import React, { useLayoutEffect } from "react";
-import { Row, Col } from "antd";
-import { auth } from "../signupLogin/auth";
+import React, { useLayoutEffect } from 'react';
+import { auth } from '../signupLogin/auth';
+import authBg from '../assets/imgs/background/authBg.jpg';
 
 const AuthPageLayout = ({ history, Component, title, ...rest }) => {
 	useLayoutEffect(() => {
 		const token = auth.getAccessToken();
 		if (token !== null) {
-			history.push("/");
+			history.push('/');
 		}
 	}, []);
 	return (
 		<div className="auth-page-layout">
+			<div style={{ width: '40%', height: '100%' }}>
+				<img src={authBg} class="responsive" />
+			</div>
+
 			<div class="auth-page-container">
 				<h1>{title}</h1>
 				<Component routerHistory={history} {...rest} />
