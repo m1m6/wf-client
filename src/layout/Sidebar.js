8b6728066ab9069bac907f1e81b77adb5bc6068a
abeeralshaer <abber2000g@gmail.com]>
2020-02-23 14:01:41 +0200

    fix prettier

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -9,92 +9,94 @@ import { SubMenu } from "rc-menu";
 const { Sider } = Layout;
 
 const HeaderLogo = () => (
-  <div className="header-logo">
-    <img src={headerLogo} />
-  </div>
+    <div className="header-logo">
+        <img src={headerLogo} />
+    </div>
 );
 
 const Sidebar = () => {
-  const { loading, error, data } = useMeQueryClient();
+    const { loading, error, data } = useMeQueryClient();
 
-  if (loading) {
-    return <Skeleton active loading paragraph />;
-  }
+    if (loading) {
+        return <Skeleton active loading paragraph />;
+    }
 
-  const {
-    me: { role }
-  } = data;
+    const {
+        me: { role }
+    } = data;
 
-  const isAdminUser = isAdmin(role);
-  const isBrandUser = isBrand(role);
-  const isCreatorUser = isCreator(role);
+    const isAdminUser = isAdmin(role);
+    const isBrandUser = isBrand(role);
+    const isCreatorUser = isCreator(role);
 
-  return (
-    <Sider width={270} className="sidebar-wrapper">
-      <HeaderLogo />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-        <SubMenu
-          className="siderbar-profile-section"
-          key="0"
-          title={
-            <span>
-              <Avatar
-                shape="square"
-                src={
-                  "https://image.shutterstock.com/image-photo/close-portrait-beautiful-young-black-260nw-623123291.jpg"
-                }
-                className="siderbar-avatar"
-              />
-              <span className="siderbar-username">Abeer Alshaer</span>
-            </span>
-          }
-        >
-          <Menu.Item key="01" className="profile-submenu-item">
-            <Link to="/profile">Profile</Link>
-          </Menu.Item>
-          <Menu.Item key="02" className="profile-submenu-item">
-            Sign out
-          </Menu.Item>
-          <Menu.Item key="03" className="profile-submenu-item">
-            Reset Password
-          </Menu.Item>
-        </SubMenu>
+    return (
+        <Sider width={270} className="sidebar-wrapper">
+            <HeaderLogo />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+                <SubMenu
+                    className="siderbar-profile-section"
+                    key="0"
+                    title={
+                        <span>
+                            <Avatar
+                                shape="square"
+                                src={
+                                    "https://image.shutterstock.com/image-photo/close-portrait-beautiful-young-black-260nw-623123291.jpg"
+                                }
+                                className="siderbar-avatar"
+                            />
+                            <span className="siderbar-username">
+                                Abeer Alshaer
+                            </span>
+                        </span>
+                    }
+                >
+                    <Menu.Item key="01" className="profile-submenu-item">
+                        <Link to="/profile">Profile</Link>
+                    </Menu.Item>
+                    <Menu.Item key="02" className="profile-submenu-item">
+                        Sign out
+                    </Menu.Item>
+                    <Menu.Item key="03" className="profile-submenu-item">
+                        Reset Password
+                    </Menu.Item>
+                </SubMenu>
 
-        <Menu.Item key="1">
-          <Icon type="bell" />
-          <Link to="/notifications">
-            <span className="nav-text">Notifications</span>
-          </Link>
-        </Menu.Item>
-        {(isBrandUser || isAdminUser) && (
-          <Menu.Item key="21">
-            <Icon type="profile" />
-            <Link to="/discover">
-              <span className="nav-text">Discover</span>
-            </Link>
-          </Menu.Item>
-        )}
+                <Menu.Item key="1">
+                    <Icon type="bell" />
+                    <Link to="/notifications">
+                        <span className="nav-text">Notifications</span>
+                    </Link>
+                </Menu.Item>
+                {(isBrandUser || isAdminUser) && (
+                    <Menu.Item key="21">
+                        <Icon type="profile" />
+                        <Link to="/discover">
+                            <span className="nav-text">Discover</span>
+                        </Link>
+                    </Menu.Item>
+                )}
 
-        {(isCreatorUser || isAdminUser) && (
-          <Menu.Item key="2">
-            <Icon type="profile" />
-            <Link to="/creator-campaigns">
-              <span className="nav-text">My Campaigns</span>
-            </Link>
-          </Menu.Item>
-        )}
+                {(isCreatorUser || isAdminUser) && (
+                    <Menu.Item key="2">
+                        <Icon type="profile" />
+                        <Link to="/creator-campaigns">
+                            <span className="nav-text">My Campaigns</span>
+                        </Link>
+                    </Menu.Item>
+                )}
 
-        {(isBrandUser || isAdminUser) && (
-          <Menu.Item key="7">
-            <Icon type="team" />
-            <Link to="/campaigns">
-              <span className="nav-text">Campaigns</span>
-            </Link>
-          </Menu.Item>
-        )}
-      </Menu>
-    </Sider>
-  );
+                {(isBrandUser || isAdminUser) && (
+                    <Menu.Item key="7">
+                        <Icon type="team" />
+                        <Link to="/campaigns">
+                            <span className="nav-text">Campaigns</span>
+                        </Link>
+                    </Menu.Item>
+                )}
+            </Menu>
+        </Sider>
+    );
 };
 
 export default Sidebar;
